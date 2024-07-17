2db2e26f099f06acff9efc541315abb4881829a4
sunil-reddy-19 <sunilhachareddy@gmail.com>
2024-07-17 20:47:31 +0530

    First commit

--- Pyspark_test.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("Test").getOrCreate()
-
-df = spark.read.csv("insurance.csv",inferSchema=True,header=True)
-
-df.show()
--- README.md
+++
@@ -1,2 +0,0 @@
-# Spark_with_Scala
-spark with scala lang
