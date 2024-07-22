58ab0eeaa42215fff47a151e2d0e9556d630b81a
sunil-reddy-19 <sunilhachareddy@gmail.com>
2024-07-22 12:54:23 +0530

    commit

---
+++ src/main/scala/firstClassObj.scala
@@ -0,0 +1,7 @@
+object firstClassObj extends App{
+  val a : Int = 10
+  def A: Int = 20
+  val b = a + A
+  println(b)
+// functions are first class objects i.e it can be passed in place of variables
+}
