440f65475190852c7933069b71fd492d87bd7f93
sunil-reddy-19 <sunilhachareddy@gmail.com>
2024-08-05 13:33:26 +0530

    commit

--- src/main/scala/Scala_Spark_1.scala
+++ src/main/scala/Scala_Spark_1.scala
@@ -1,7 +1,11 @@
 import org.apache.hadoop.shaded.org.eclipse.jetty.websocket.common.frames.DataFrame
-import org.apache.spark.sql.{Dataset, SparkSession}
+import org.apache.spark.sql.{Dataset, SparkSession,Row}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.Column
+import org.apache.spark.sql.functions.{col, explode, explode_outer}
+import org.apache.spark.sql.types.{ArrayType, MapType, StringType, StructType}
+
+
 
 object Scala_Spark_1 {
 
@@ -20,7 +24,11 @@ object Scala_Spark_1 {
 
   def main(args:Array[String]): Unit = {
 
+
+
     val spark = spark_init()
+
+    import spark.implicits._
 
     val filename="/Users/sunil_reddy/Spark/input_data/datasets/dw_dataset/sales_1.csv"
 
@@ -41,6 +49,35 @@ object Scala_Spark_1 {
     df_2.show()
 
     df_2.printSchema()
+
+    val arrayData = Seq(
+      Row("James",List("Java","Scala"),Map("hair"->"black","eye"->"brown")),
+      Row("Michael",List("Spark","Java",null),Map("hair"->"brown","eye"->null)),
+      Row("Robert",List("CSharp",""),Map("hair"->"red","eye"->"")),
+      Row("Washington",null,null),
+      Row("Jefferson",List(),Map())
+    )
+
+    val arraySchema = new StructType()
+      .add("name",StringType)
+      .add("knownLanguages", ArrayType(StringType))
+      .add("properties", MapType(StringType,StringType))
+
+    val df = spark.createDataFrame(spark.sparkContext.parallelize(arrayData),arraySchema)
+
+    df.show()
+
+    val df11 = df.select($"name",explode($"knownLanguages").as("knownLanguages"),$"properties")
+    df1.show()
+    val df2 = df11.select($"name",$"knownLanguages",explode($"properties"))
+    val df3 = df2.withColumnRenamed("key","attributte").withColumnRenamed("value","color")
+
+    df11.select($"name",$"knownLanguages",$"properties.hair".as("hair_color"),$"properties.eye".as("eye_color")).show()
+
+
+
+
+
 
 
 
