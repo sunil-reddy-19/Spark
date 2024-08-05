29d9365999a4e6d57c53266b71e608321c981dc3
sunil-reddy-19 <sunilhachareddy@gmail.com>
2024-08-05 22:54:32 +0530

    Commit

--- Pyspark_1.py
+++ Pyspark_1.py
@@ -1,5 +1,6 @@
 import pyspark.sql
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
 import pandas as pd
 
 def spark_init():
@@ -15,3 +16,19 @@ spark = spark_init()
 file_name = "/Users/sunil_reddy/PycharmProjects/Pyspark/Input_Files/sales_1.csv"
 df = read_input_file(file_name,spark)
 df.show()
+
+df.select("item_id","item_qty",date_format("date_of_sale","dd-MM-yyyy").alias("New_date_format_sales")).show()
+
+data = [[1,"abc",{"hair":"black","eye":"brown"},[1000,10000]],
+        [2,"def",{"hair":"white","eye":"black"},[2000,20000]],
+        [3,"xyz",{"hair":"red","eye":"red"},[3000,30000]]]
+print(data)
+
+columns = ["ID","NAME","Attribute","salary"]
+
+df_1 = spark.createDataFrame(data,columns)
+
+df_1.show()
+
+df_2 = df_1.select("ID","NAME",explode("Attribute"))
+df_2.withColumnsRenamed({"key":"attribute","value":"color"}).show()
